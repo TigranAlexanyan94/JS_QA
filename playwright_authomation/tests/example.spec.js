a2bef187ec7970fad18898040a90dde2e308d3c3
TigranAlexanyan94 <tigran.alexanyan.im@gmail.com>
2023-02-08 13:10:09 +0400

    Task

--- playwright_authomation/tests/example.spec.js
+++ playwright_authomation/tests/example.spec.js
@@ -5,9 +5,15 @@ const { PlaywrightDevPage } = require('./playwright-dev-page');
 test('Test for website buy.am header', async ({ page }) => {
   const playwrightDev = new PlaywrightDevPage(page);
   await playwrightDev.goto();
+  await playwrightDev.clicklogoButton();
+  await playwrightDev.getTelNumberText();
+  await playwrightDev.getLogoInfoText();
+  await playwrightDev.clickSearchCategorySelection();
+  await playwrightDev.searchFieldText();
+  await playwrightDev.clickSearchBtn();
   await playwrightDev.clickWishList();
   await playwrightDev.clickChart();
-  await playwrightDev.clickSettings()
-  // await playwrightDev.gotoSuperMarket()
+  await playwrightDev.clickAccount();
+  await playwrightDev.clickSettings();
 });
 
--- playwright_authomation/tests/playwright-dev-page.js
+++ playwright_authomation/tests/playwright-dev-page.js
@@ -4,14 +4,44 @@ exports.PlaywrightDevPage = class PlaywrightDevPage {
    */
   constructor(page) {
     this.page = page;
+    this.logoBtn = page.locator('.logo--shop > .logo--link>>nth=0')
+    this.telNumber = page.locator('.logo--supportinfo > .tel-number')
+    this.logoInfoText = page.locator('.logo--supportinfo > .support-info-text')
+    this.searchCategorySelectOption = page.locator('.search-container > .main-search--form > #searchCategorySelectionBox')
+    this.searchField = page.locator('.main-search--field')
+    this.serchBtn = page.locator('.main-search--button')
     this.wishList = page.locator('.entry--notepad > .btn')
     this.chart = page.locator('.entry--cart')
-    this.settings = page.locator('.entry--shop-settings')
-    //this.supermarket = page.locator(".navigation-main > .container > .navigation--list-wrapper > .navigation--list > .navigation--entry > .desktop-nav-bg")
+    this.account = page.locator('.btn>>nth=6')
+    this.settings = page.locator('.entry--shop-settings > .btn')
   }
 
   async goto() {
     await this.page.goto('https://buy.am/en');
+  }
+
+  async clicklogoButton(){
+    await this.logoBtn.click()
+  }
+
+  async getTelNumberText(){
+    await this.telNumber.getByText('+374 10 311 111')
+  }
+
+  async getLogoInfoText(){
+    await this.logoInfoText.getByText('Զանգերի կենտրոն')
+  }
+
+  async clickSearchCategorySelection(){
+    await this.searchCategorySelectOption.click()
+  }
+
+  async searchFieldText(){
+    await this.searchField.fill('սուպերմարկետ')
+  }
+
+  async clickSearchBtn(){
+    await this.serchBtn.click()
   }
 
   async clickWishList(){
@@ -22,19 +52,25 @@ exports.PlaywrightDevPage = class PlaywrightDevPage {
     await this.chart.click()
   }
 
+  async clickAccount(){
+    await this.account.click()
+  }
+
   async clickSettings(){
+    console.log(this.settings.innerHTML)
     await this.settings.click()
   }
 
-  // async gotoSuperMarket(){
-  //   await this.supermarket.click()
-  // }
-
   async pageObjectModel(){
     await this.goto()
+    await this.clicklogoButton()
+    await this.getTelNumberText()
+    await this.getLogoInfoText()
+    await this.clickSearchCategorySelection()
+    await this.searchFieldText()
     await this.clickWishList()
     await this.clickChart()
+    await this.clickUser()
     await this.clickSettings()
-    // await this.gotoSuperMarket()
   }
 }
